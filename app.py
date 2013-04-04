876b4d8feb30e60633c589fb994619c420b1d9d2
Andrews Medina <andrewsmedina@gmail.com>
2013-04-04 19:58:43 -0300

    added view for confirmation page.

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     return render_template("index.html"), 200
 
 
+@app.route("/confirmation")
+def confirmation():
+    return render_template("confirmation.html"), 200
+
+
 if __name__ == "__main__":
     app.run()
--- test_app.py
+++ test_app.py
@@ -7,8 +7,12 @@ class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.api = app.test_client()
 
-    def test_should_get_and_be_success(self):
+    def test_should_get_index_and_be_success(self):
         resp = self.api.get("/")
+        self.assertEqual(200, resp.status_code)
+
+    def test_should_get_confirmation_and_be_success(self):
+        resp = self.api.get("/confirmation")
         self.assertEqual(200, resp.status_code)
 
     def test_should_have_facebook_login_button_in_content(self):
