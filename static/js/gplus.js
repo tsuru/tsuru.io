3f647eb8ab1b0068be242e9043bc2ff3af395795
Francisco Souza <fss@corp.globo.com>
2013-04-05 17:31:17 -0300

    js/gplus: also send the token_type to the server

--- static/js/gplus.js
+++ static/js/gplus.js
@@ -24,6 +24,6 @@ function renderGplus() {
 
 function gplusCallback(r) {
 	if(r.access_token) {
-		window.location.href = "/register/gplus?token=" + r.access_token;
+		window.location.href = "/register/gplus?token=" + r.access_token + "&token_type=" + r.token_type;
 	}
 }
