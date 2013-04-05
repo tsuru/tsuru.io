caba8430fbe697344bf14e2deded72cd460d1e8e
Francisco Souza <fss@corp.globo.com>
2013-04-05 15:43:30 -0300

    all: add license information

--- static/js/gplus.js
+++ static/js/gplus.js
@@ -1,3 +1,7 @@
+// Copyright 2013 Globo.com. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 (function() {
 	var po = document.createElement('script');
 	po.type = 'text/javascript';
