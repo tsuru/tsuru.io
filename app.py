02fe3da516478817268531f487e9f6ed3b1c46db
Flavia Missi <flaviamissi@gmail.com>
2013-04-05 11:37:41 -0300

    app: draft of register user using facebook. PS: the data saved will change

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+import os
+import pymongo
+from flask import Flask, render_template, g, request
 
 app = Flask(__name__)
+MONGO_URI = os.environ.get("MONGO_URI", "localhost:27017")
+MONGO_USER = os.environ.get("MONGO_USER", "")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
+MONGO_DATABASE_NAME = os.environ.get("MONGO_DATABASE_NAME", "test")
 
 
 @app.route("/")
@@ -13,9 +19,42 @@ def confirmation():
     return render_template("confirmation.html"), 200
 
 
-@app.route("/facebook-login", methods=["POST"])
+@app.route("/login/facebook", methods=["POST"])
 def facebook_login():
+    if not is_login_valid(request.form):
+        return "Missing required fields for login.", 400
+    user = {"first_name": request.form["first_name"],
+            "last_name": request.form["last_name"]}
+    g.db.users.insert(user)
     return "", 201
+
+
+def is_login_valid(form):
+    if "first_name" not in form.keys() or "last_name" not in form.keys():
+        return False
+    if not form["first_name"] or form["first_name"] == "" or not form["last_name"] \
+       or form["last_name"] == "":
+        return False
+    return True
+
+
+@app.before_request
+def before_request():
+    g.conn, g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    g.conn.close()
+
+
+def connect_db():
+    mongo_uri_port = MONGO_URI.split(":")
+    host = mongo_uri_port[0]
+    port = int(mongo_uri_port[1])
+    conn = pymongo.Connection(host, port)
+    return conn, conn[MONGO_DATABASE_NAME]
+
 
 if __name__ == "__main__":
     app.run()
--- test_app.py
+++ test_app.py
@@ -1,12 +1,14 @@
+import os
 import unittest
-from app import app
+import app
+from pymongo import Connection
 
 
 class ClientTest(object):
 
     @classmethod
     def setUpClass(self):
-        self.api = app.test_client()
+        self.api = app.app.test_client()
 
 
 class AppTestCase(ClientTest, unittest.TestCase):
@@ -26,9 +28,56 @@ class AppTestCase(ClientTest, unittest.TestCase):
 
 class FacebookLoginTestCase(ClientTest, unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        mongo_uri_port = app.MONGO_URI.split(":")
+        host = mongo_uri_port[0]
+        port = int(mongo_uri_port[1])
+        cls.conn = Connection(host, port)
+        cls.db = cls.conn[app.MONGO_DATABASE_NAME]
+        ClientTest.setUpClass()
+
+    def tearDown(self):
+        self.db.users.remove()
+
     def test_should_receive_facebook_data_and_store_on_database(self):
-        resp = self.api.post("/facebook-login")
+        data = {"first_name": "First", "last_name": "Last"}
+        resp = self.api.post("/login/facebook", data=data)
         self.assertEqual(201, resp.status_code)
+        u = self.db.users.find_one({"first_name": "First", "last_name": "Last"})
+        self.assertIsNotNone(u)
+        expected = {"first_name": "First", "last_name": "Last"}
+        self.assertEqual(expected["first_name"], u["first_name"])
+        self.assertEqual(expected["last_name"], u["last_name"])
+
+    def test_should_return_400_and_do_not_save_user_when_validation_fails(self):
+        data = {"first_name": "First", "last_name": ""}
+        resp = self.api.post("/login/facebook", data=data)
+        self.assertEqual(400, resp.status_code)
+        u = self.db.users.find_one({"first_name": "First"})
+        self.assertIsNone(u)
+
+
+class GithubLoginTestCase(ClientTest, unittest.TestCase):
+
+    pass
+
+
+class HelperTests(unittest.TestCase):
+
+    def test_is_login_valid_should_check_for_first_name_and_last_name(self):
+        is_valid = app.is_login_valid({"first_name": "First", "last_name": "Last"})
+        self.assertTrue(is_valid)
+
+    def test_valida_login_should_return_false_when_there_is_no_last_name(self):
+        is_valid = app.is_login_valid({"first_name": "First"})
+        self.assertFalse(is_valid)
+
+    def test_valid_login_should_return_false_when_one_value_is_empty(self):
+        is_valid = app.is_login_valid({"first_name": "First", "last_name": ""})
+        self.assertFalse(is_valid)
+        is_valid = app.is_login_valid({"first_name": "", "last_name": "Last"})
+        self.assertFalse(is_valid)
 
 if __name__ == "__main__":
     unittest.main()
