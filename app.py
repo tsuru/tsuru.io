79917b8e86dc61e9e05f672d95fcde2295fa37c9
Andrews Medina <andrewsmedina@gmail.com>
2013-04-08 11:51:48 -0300

    fixed tests.

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "secret")
 
 babel = Babel(app)
-csrf.csrf(app)
 
 MONGO_URI = os.environ.get("MONGO_URI", "localhost:27017")
 MONGO_USER = os.environ.get("MONGO_USER", "")
