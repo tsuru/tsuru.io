dcafcfe0b9e3e472dac782e355cdbb2c5310e6d9
Flavia Missi <flaviamissi@gmail.com>
2013-04-08 11:57:23 -0300

    forms: started survet wtform
    Also stopped using wtform.validators.Required, from wtform doc:
    "This class will start throwing deprecation warnings in WTForms 1.1 and
    be removed by 1.2."

--- forms.py
+++ forms.py
@@ -5,8 +5,16 @@ from wtforms import validators
 
 class SignupForm(wtf.Form):
     first_name = wtforms.TextField(u"First name",
-                                   validators=[validators.Required()])
+                                   validators=[validators.DataRequired()])
     last_name = wtforms.TextField(u"Last name",
-                                  validators=[validators.Required()])
-    email = wtforms.TextField(u"Email", validators=[validators.Required(),
+                                  validators=[validators.DataRequired()])
+    email = wtforms.TextField(u"Email", validators=[validators.DataRequired(),
                                                     validators.Email()])
+
+
+class SurveyForm(wtf.Form):
+    email = wtforms.HiddenField(u"email",
+                                validators=[validators.DataRequired(),
+                                            validators.Email()])
+    signature = wtforms.HiddenField(u"signature",
+                                    validators=[validators.DataRequired()])
--- test_forms.py
+++ test_forms.py
@@ -7,6 +7,12 @@ from wtforms import validators
 import forms
 
 
+class SurveyFormTestCase(unittest.TestCase):
+
+    def test_should_be_a_wtform(self):
+        assert issubclass(forms.SurveyForm, wtf.Form)
+
+
 class SignupFormTestCase(unittest.TestCase):
 
     def test_should_be_a_wtform(self):
@@ -23,7 +29,7 @@ class SignupFormTestCase(unittest.TestCase):
     def test_first_name_should_be_mandatory(self):
         field = forms.SignupForm.first_name
         self.assertIsInstance(field.kwargs["validators"][0],
-                              validators.Required)
+                              validators.DataRequired)
 
     def test_last_name_should_be_text_field(self):
         field = forms.SignupForm.last_name
@@ -36,7 +42,7 @@ class SignupFormTestCase(unittest.TestCase):
     def test_last_name_should_be_mandatory(self):
         field = forms.SignupForm.last_name
         self.assertIsInstance(field.kwargs["validators"][0],
-                              validators.Required)
+                              validators.DataRequired)
 
     def test_email_should_be_text_field(self):
         field = forms.SignupForm.email
@@ -49,7 +55,7 @@ class SignupFormTestCase(unittest.TestCase):
     def test_email_should_be_mandatory(self):
         field = forms.SignupForm.email
         self.assertIsInstance(field.kwargs["validators"][0],
-                              validators.Required)
+                              validators.DataRequired)
 
     def test_email_should_require_valid_email(self):
         field = forms.SignupForm.email
