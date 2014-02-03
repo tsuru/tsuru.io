6cac3c40ec5cb3472ae7fd2e235fbb52ae8111c9
Andrews Medina <andrewsmedina@gmail.com>
2014-02-03 01:34:10 -0200

    added gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: [{
+					expand: true,
+					cdw: 'resources/styles',
+				  src: '*.scss',
+					dest: 'resources/styles',
+					ext: '.css'
+				}]
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
