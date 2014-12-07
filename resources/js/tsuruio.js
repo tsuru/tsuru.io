8a01f2ce117bfc63e3a9973e524e0e950ae91203
Andrews Medina <andrewsmedina@gmail.com>
2014-12-07 18:01:43 -0200

    implemented try tsuru button.

--- resources/js/tsuruio.js
+++ resources/js/tsuruio.js
@@ -1,15 +1,6 @@
 $(function() {
-	function goTo($element) {
-		console.log($element);
-		var headerHeight = 112;
-		$('html, body').animate({scrollTop: $element.offset().top - 112}, 1000);
-	}
-
-	$.each($(".navigation .scroll-btn"), function(index, element) {
-		$(this).on('click', function(event) {
-			event.preventDefault();
-			goTo($($(this).attr("href")));
-		});
-	});
-
+  $(".try .button").on("click", function(e) {
+    e.preventDefault();
+    $(".dropdown-menu").toggleClass("open");
+  });
 });
